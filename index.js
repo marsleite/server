ee82ec3f562af78899bcd04b57abeaa803d17ae7
Marcelo <marsleite@gmail.com>
2021-11-02 19:55:11 -0300

    conexão com o mongodb cloud

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const app = express();
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://admin:928010Mgr@cluster0.uvgnu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
+  .catch(err => console.log(err.message));
