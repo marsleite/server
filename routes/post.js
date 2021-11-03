e6f5439be1ef87d66129a372d01ddcc32f0e8cb4
Marcelo <marsleite@gmail.com>
2021-11-02 21:07:09 -0300

    configuração do PostSchema, controllers getpost e createPost, criação da routes post

---
+++ routes/post.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const Post = require('../controllers/posts');
+
+router.get('/', Post.getposts);
+// router.post('/', Post.createPost);
+
+module.exports = router;
