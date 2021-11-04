8ac832ad4d047c798266488ab2083e2fefad2e51
Marcelo <marsleite@gmail.com>
2021-11-03 21:12:43 -0300

    retornar com a rota

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const Post = require('../controllers/posts');
 
 router.get('/', Post.getposts);
-// router.post('/', Post.createPost);
+router.post('/', Post.createPost);
 
 module.exports = router;
